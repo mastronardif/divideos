47bca4d9b046a37dafeefaf9b68e1a89ceb0d273
mastronardif <mastronardif@gmail.com>
2019-05-23 13:07:34 -0400

    cleanup

--- src/app/shared/pipes/playlist-item-dragdata.pipe.ts
+++ src/app/shared/pipes/playlist-item-dragdata.pipe.ts
@@ -6,13 +6,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 export class PlaylistItemDragDataPipe implements PipeTransform {
   transform(value: any, args?: any[]): any {
-    // const dots = '...';
-
-    // if (value && value.length > 65) {
-    //   value = value.substring(0, 62) + dots;
-    // }
-    // return `draind the sqampe ${value.snippet.title}`;
-    //return {fuck: 'you'}; //value;
     const i = value.id;
     let myDragableObjects33  = {
       data: {
@@ -20,7 +13,7 @@ export class PlaylistItemDragDataPipe implements PipeTransform {
         payload: `<figure class="image">
         <img src="${value.snippet.thumbnails.default.url}" alt="">
         </figure>
-        <a href="https://www.youtube.com/watch?v=${i}}">${value.snippet.title}</a>        
+        <a href="https://www.youtube.com/watch?v=${i}}">${value.snippet.title}</a>
         `,
         name: 'My Draggable - ' + i,
         currentColumn: i,
